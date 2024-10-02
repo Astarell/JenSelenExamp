f440a6dd65cad5c8b3fceaa6e60949695c0f53ce
Astarell <astarell1@yandex.ru>
2024-10-02 14:31:09 +0300

    Added master mes

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -6,12 +6,12 @@ public class Test1 {
     @Test
     @Tag("Group1")
     public void test1(){
-        System.out.println("Test 1");
+        System.out.println("Test 1 MASTER");
     }
 
     @Test
     @Tag("Group2")
     public void test2(){
-        System.out.println("Test 2");
+        System.out.println("Test 2 MASTER");
     }
 }
