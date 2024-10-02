e230f13e3d569eded42dc23c04a25ec4656a38d6
Astarell <astarell1@yandex.ru>
2024-10-02 11:43:21 +0300

    Initial commit

---
+++ src/test/java/Test1.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class Test1 {
+
+    @Test
+    @Tag("Group1")
+    public void test1(){
+        System.out.println("Test 1");
+    }
+
+    @Test
+    @Tag("Group2")
+    public void test2(){
+        System.out.println("Test 2");
+    }
+}
---
+++ src/test/java/Test2.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class Test2 {
+
+    @Test
+    @Tag("Group1")
+    public void test1(){
+        System.out.println("Test 3");
+    }
+
+    @Test
+    @Tag("Group2")
+    public void test2(){
+        System.out.println("Test 4");
+    }
+}
